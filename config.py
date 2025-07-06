e092b94e867747af0ec37bc93da5a4556467d51a
Christian <csells10@gmail.com>
2025-07-05 20:19:33 -0400

    config

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ from api_calls.api_call_nfl_scores import fetch_nfl_scores
 
 # List of API calls and their schedules
 API_CALLS = [
-    # {
-    #     'name': 'NFL Game Schedule API Call',
-    #     'function': fetch_nfl_games,
-    #     'max_cycles': 1
-    # },
+    {
+        'name': 'NFL Game Schedule API Call',
+        'function': fetch_nfl_games,
+        'max_cycles': 1
+    },
     {
         'name': 'NFL Stats API Call',
         'function': fetch_nfl_stats,
         'max_cycles': 1
-    }
-    # {
-    #     'name': 'NFL Scores API Call',
-    #     'function': fetch_nfl_scores,
-    #     'max_cycles': 1
-    # }
+    },
+    {
+        'name': 'NFL Scores API Call',
+        'function': fetch_nfl_scores,
+        'max_cycles': 1    
+     }
 ]
